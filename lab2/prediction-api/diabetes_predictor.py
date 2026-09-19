ded75d4c58786211283859e7d2aabb75eeb34682
Indika Kumara <ipkumarawd@yahoo.com>
2026-09-19 12:22:20 +0200

    Updated Lab 2 Code

--- lab2/prediction-api/diabetes_predictor.py
+++ lab2/prediction-api/diabetes_predictor.py
@@ -3,10 +3,9 @@ import os
 
 import pandas as pd
 from flask import jsonify
-from keras.models import load_model
 import logging
 from io import StringIO
-
+import pickle
 
 class DiabetesPredictor:
     def __init__(self):
@@ -16,17 +15,22 @@ class DiabetesPredictor:
         logging.debug(prediction_input)
         if self.model is None:
             try:
+                # load the model from a directory /folder given in the environment variables MODEL_REPO
+                # assume the model file name as model.pkl
                 model_repo = os.environ['MODEL_REPO']
-                file_path = os.path.join(model_repo, "model.keras")
-                self.model = load_model(file_path)
+                file_path = os.path.join(model_repo, "model.pkl")
+                self.model =  pickle.load(open(file_path, 'rb'))
             except KeyError:
                 print("MODEL_REPO is undefined")
-                self.model = load_model('model.keras')
+                # Otherwise, use the local model (in prediction-api folder)
+                self.model = pickle.load(open('model.pkl', 'rb'))
 
         df = pd.read_json(StringIO(json.dumps(prediction_input)), orient='records')
-        y_pred = self.model.predict(df)
+        xNew = df[['ntp', 'pgc', 'dbp', 'tsft', 'si', 'bmi', 'dpf', 'age']]
+        y_pred = self.model.predict(xNew)
+        print(y_pred)
         logging.info(y_pred[0])
         status = (y_pred[0] > 0.5)
-        logging.info(type(status[0]))
+        print(status)
         # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
-        return jsonify({'result': str(status[0])}), 200
+        return jsonify({'result': str(status)}), 200
